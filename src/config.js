acd4f2e97c0f32cf7be79a6d41fcebae26a1ce3a
Shari Hobbs <sharirhobbs@gmail.com>
2018-05-25 23:54:44 -0700

    default api url to netlify and override in local dev

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 module.exports = {
   PORT: process.env.PORT || 3000,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    'http://localhost:3001/api'
+    'https://quiet-springs-62758.herokuapp.com'
 };
